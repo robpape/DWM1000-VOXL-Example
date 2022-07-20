b881e85fd1856c16f669222c40e1b89dd15476eb
Robin Pape <robin@robinpape.de>
2022-07-19 17:11:05 -0700

    Initial commit

---
+++ boards/voxl_dw1000.h
@@ -0,0 +1,11 @@
+#ifndef VOXL_DW1000_H
+#define VOXL_DW1000_H
+
+#define DW1000_RST    4
+
+#define SPIS_MISO_PIN   3  // SPI MISO signal.
+#define SPIS_CSN_PIN    9  // SPI CSN signal.
+#define SPIS_MOSI_PIN   2  // SPI MOSI signal.
+#define SPIS_SCK_PIN    10  // SPI SCK signal.
+
+#endif
